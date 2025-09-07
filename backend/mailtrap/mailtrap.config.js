274d052a00e7ad02fefe60e9494d280185d2f0c4
nael-awadallah <nael.awadallah@gmail.com>
2025-09-07 10:08:58 +0300

    MailTrap

--- backend/mailtrap/mailtrap.config.js
+++ backend/mailtrap/mailtrap.config.js
@@ -13,18 +13,3 @@ export const sender = {
   email: "hello@nael-portfolio.site",
   name: "Nael Portfolio Website",
 };
-// const recipients = [
-//   {
-//     email: "nael.awadallah@gmail.com",
-//   },
-// ];
-
-// client
-//   .send({
-//     from: sender,
-//     to: recipients,
-//     subject: "You are awesome!",
-//     text: "Congrats for sending test email with Mailtrap!",
-//     category: "Integration Test",
-//   })
-//   .then(console.log, console.error);
